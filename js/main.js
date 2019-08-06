f6994028a58817ffdb2a172300814f1650f4a57c
Laura Pareja <lauraparejalmodovar@gmail.com>
2019-08-07 00:57:17 +0200

    1/ Listado de noticias resuelto

--- js/main.js
+++ js/main.js
@@ -1,24 +1,46 @@
 'use strict';
 
-const data = [
-  {
-    title: 'Asteroids 101',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Life on Mars',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Martians invade Earth',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Humans aren\'t real',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Space The Final Frontier',
-    image: 'https://via.placeholder.com/200x100'
-  }
+const data = [{
+        title: 'Asteroids 101',
+        image: 'https://via.placeholder.com/200x100'
+    },
+    {
+        title: 'Life on Mars',
+        image: 'https://via.placeholder.com/200x100'
+    },
+    {
+        title: 'Martians invade Earth',
+        image: 'https://via.placeholder.com/200x100'
+    },
+    {
+        title: 'Humans aren\'t real',
+        image: 'https://via.placeholder.com/200x100'
+    },
+    {
+        title: 'Space The Final Frontier',
+        image: 'https://via.placeholder.com/200x100'
+    }
 ];
+
+
+const listNews = document.querySelector('.news');
+
+for (let i = 0; i < data.length; i++) {
+
+    const newItemList = document.createElement('li');
+    const newTitle = document.createElement('h2');
+    const newContentTitle = document.createTextNode(data[i].title);
+    const newImage = document.createElement('img');
+
+    newItemList.className = '.new__item';
+    newTitle.appendChild(newContentTitle);
+    newTitle.className = '.news__title';
+
+    newImage.src = data[i].image;
+    newImage.alt = data[i].title;
+
+    listNews.appendChild(newItemList);
+    listNews.appendChild(newTitle);
+    listNews.appendChild(newImage);
+
+}
