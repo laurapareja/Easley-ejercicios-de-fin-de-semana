665934ee33b492b7b626e7566374308c90afbf9c
Laura Pareja <lauraparejalmodovar@gmail.com>
2019-08-07 09:41:14 +0200

    3/ En el espacio nadie puede oir tus fetchs

--- js/main.js
+++ js/main.js
@@ -1,50 +1,57 @@
 'use strict';
 
-const data = [{
-        title: 'Asteroids 101',
-        image: 'https://via.placeholder.com/200x100'
-    },
-    {
-        title: 'Life on Mars',
-        image: 'https://via.placeholder.com/200x100'
-    },
-    {
-        title: 'Martians invade Earth',
-        image: 'https://via.placeholder.com/200x100'
-    },
-    {
-        title: 'Humans aren\'t real',
-        image: 'https://via.placeholder.com/200x100'
-    },
-    {
-        title: 'Space The Final Frontier',
-        image: 'https://via.placeholder.com/200x100'
-    }
-];
+// const data = [{
+//         title: 'Asteroids 101',
+//         image: 'https://via.placeholder.com/200x100'
+//     },
+//     {
+//         title: 'Life on Mars',
+//         image: 'https://via.placeholder.com/200x100'
+//     },
+//     {
+//         title: 'Martians invade Earth',
+//         image: 'https://via.placeholder.com/200x100'
+//     },
+//     {
+//         title: 'Humans aren\'t real',
+//         image: 'https://via.placeholder.com/200x100'
+//     },
+//     {
+//         title: 'Space The Final Frontier',
+//         image: 'https://via.placeholder.com/200x100'
+//     }
+// ];
 
+fetch("https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        const listNews = document.querySelector('.news');
+        const news = data.news;
 
-const listNews = document.querySelector('.news');
+        for (let i = 0; i < news.length; i++) {
 
-for (let i = 0; i < data.length; i++) {
+            const newItemList = document.createElement('li');
+            const newTitle = document.createElement('h2');
+            const newContentTitle = document.createTextNode(news[i].title);
+            const newImage = document.createElement('img');
 
-    const newItemList = document.createElement('li');
-    const newTitle = document.createElement('h2');
-    const newContentTitle = document.createTextNode(data[i].title);
-    const newImage = document.createElement('img');
+            newItemList.className = '.new__item';
+            newTitle.appendChild(newContentTitle);
+            newTitle.className = '.news__title';
 
-    newItemList.className = '.new__item';
-    newTitle.appendChild(newContentTitle);
-    newTitle.className = '.news__title';
+            // Exercise 2. Class mars of Mars/Martian titles
+            if (news[i].title.includes("Mars") || news[i].title.includes("Martian")) {
+                newItemList.classList.add("news__item--from-mars");
+            }
 
-    // Exercise 2. Class mars of Mars/Martian titles
-    if (data[i].title.includes("Mars") || data[i].title.includes("Martian")) {
-        newItemList.classList.add("news__item--from-mars");
-    }
+            newImage.src = news[i].image;
+            newImage.alt = news[i].title;
 
-    newImage.src = data[i].image;
-    newImage.alt = data[i].title;
+            listNews.appendChild(newItemList);
+            newItemList.appendChild(newTitle);
+            newItemList.appendChild(newImage);
+        }
 
-    listNews.appendChild(newItemList);
-    newItemList.appendChild(newTitle);
-    newItemList.appendChild(newImage);
-}
+    })
